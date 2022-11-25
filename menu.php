cc77e133464a5974063032a09ae26fc36dd4f55e
ANDERSON DE PAULA <74189939+anderson-tec445@users.noreply.github.com>
2022-11-25 06:54:56 -0300

    comit

---
+++ menu.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Dev.Org</title>
+	<link href="css/estilo.css" rel="stylesheet" type="text/css" />
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+  <link rel="shortcut icon" type="image/x-icon" href="imagens/escola.png" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+	<!-- Este código abaixo é para importar a classe de icones -->
+	<link href="https://file.myfontastic.com/mhSBD4FxMhWWkou6Gm7T2V/icons.css" rel="stylesheet">
+
+</head>
+<body>
+
+
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">Dev.Org</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="cursos.php">Cursos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="usuarios.php">Usuários</a>
+      </li>
+     
+      <li class="nav-item">
+        <a class="nav-link disabled" href="contatos.php">Contatos <span class="sr-only">(current)</span> </a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Buscar" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+    </form>
+  </div>
+</nav>
